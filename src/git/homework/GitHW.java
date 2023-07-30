e485aa1e1c783426ac24b903db46e5c1ba290223
Jefreey A <Jefreey A@DESKTOP-DN6I8K8>
2023-07-29 21:36:17 -0400

    initial class

---
+++ src/git/homework/GitHW.java
@@ -0,0 +1,15 @@
+package git.homework;
+
+public class GitHW {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("This is another hw day");
+	}
+	
+	public static void add(int a, int b) {
+		
+		System.out.println(a+b);
+	}
+
+}
